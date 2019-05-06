92babf79ef5f85efeee857f0b00e7f1a876d3957
Vitor Salgado <salgado.v@pt.zetes.com>
2019-05-06 19:42:17 +0100

    POM commit

---
+++ ZetesVulcan.Test.BackOffice/PageAuthentication.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZetesVulcan.Test.BackOffice
+{
+    public class AuthenticationPage
+    {
+        private IWebDriver _driver;
+        By username = By.Id("Username");
+        By password = By.Id("Password");
+        By buttonbtnprimary = By.ClassName("btn-primary");
+
+        public void TypeUsername()
+        {
+            _driver.FindElement(username).SendKeys("");
+        }
+
+        public void TypePassword()
+        {
+            _driver.FindElement(password).SendKeys("");
+        }
+
+        public void SetButtonPrimary()
+        {
+            _driver.FindElement(buttonbtnprimary).Submit();
+        }
+    }
+}
