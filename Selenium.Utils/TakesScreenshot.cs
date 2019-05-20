2e049eb78cad2f3801e92f9830b09e7b192d4c43
Vitor Salgado <salgado.v@pt.zetes.com>
2019-05-20 17:22:05 +0100

    screenshot commit

--- Selenium.Utils/TakesScreenshot.cs
+++ Selenium.Utils/TakesScreenshot.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+
+
 
 namespace Selenium.Utils
 {
@@ -13,8 +17,17 @@ namespace Selenium.Utils
         public static string Capture(IWebDriver webDriver, string pathScreenshot, string nameScreenshot)
         {
             ITakesScreenshot ts = (ITakesScreenshot)webDriver;
-            Screenshot screenshot = ts.GetScreenshot();
-            string path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            Screenshot screenshot = ts.GetScreenshot();            
+
+            string codeBase = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+
+            if (!Directory.Exists(path.Substring(0, path.LastIndexOf("bin")) + pathScreenshot))
+            {
+                Directory.CreateDirectory((path.Substring(0, path.LastIndexOf("bin")) + pathScreenshot).ToString());
+            }
+
             string uptobinPath = path.Substring(0, path.LastIndexOf("bin")) + pathScreenshot + nameScreenshot + ".png";
             string localPath = new Uri(uptobinPath).LocalPath;
             screenshot.SaveAsFile(localPath, ScreenshotImageFormat.Png);
